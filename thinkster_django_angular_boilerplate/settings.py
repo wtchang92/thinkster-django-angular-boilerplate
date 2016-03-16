760d66f2b86532276ce3275788908329d1f42e4a
wtchang92 <wtchang051592@gmail.com>
2016-03-16 01:15:58 -0400

    start template with auth initial commit

--- thinkster_django_angular_boilerplate/settings.py
+++ thinkster_django_angular_boilerplate/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'rest_framework',
     'compressor',
+
+    #MyApps
+    'authentication',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -113,3 +116,5 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+
+AUTH_USER_MODEL = 'authentication.Account'
--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,9 +1,33 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 
 from thinkster_django_angular_boilerplate.views import IndexView
 
-urlpatterns = patterns(
-    '',
+from rest_framework_nested import routers
 
-    url('^.*$', IndexView.as_view(), name='index'),
+from authentication.views import AccountViewSet
+
+from authentication.views import LoginView
+
+from authentication.views import LogoutView
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+
+from rest_framework.routers import DefaultRouter
+
+
+# Create a router and register our viewsets with it.
+# router = DefaultRouter()
+# router.register(r'api_accounts', AccountViewSet)
+
+urlpatterns = patterns(
+     '',
+    # ... URLs
+    url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
+    url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
+
+    url('^.*$', IndexView.as_view(), name='index'), #This is known as a passthrough or catch-all route. It accepts all requests not matched by a previous rule and passes the request through to AngularJS's router for processing. The order of other URLS is normally insignificant.
+    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
+
